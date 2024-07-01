d6c6c7be9a2f9d4142b8aa6fdfcd647f4ca2e735
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-01 20:42:37 +0530

    46.08

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import {Box} from '@mui/material';
 
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Exercises from '../components/Exercises';
+
 
 
 const Home = () => {
+
   return (
     <Box>
       <HeroBanner />
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,44 @@
-import React from 'react'
-
+import React, {useEffect, useState}from 'react';
+import {Box, Button, Stack, TextField, Typography} from 
+'@mui/material';
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='center' mt='37px'
+    justifyContent='center' p='20px'>
+      <Typography fontWeight={700} sx={{
+        fontSize: {lg: '44px', xs:'30px'}
+      }}mb='50px' textAlign='center'>
+        Awesome Exercises you <br />
+        Should Know
+       </Typography>
+       <Box position='relative'mb='72px'>
+        <TextField 
+        sx={{
+          input:{fontWeight: '700', 
+            border:'none', 
+            borderRadius: '4px'},
+            width: {lg: '1170px', xs: '350px'}
+            ,
+            backgroundcolor: '#fff',
+            borderRadius:'40px'
+        }}
+          height='76px'
+          value=''
+          onChange={(e)=> {}}
+          placeholder='Search Exercises'
+          type='text'
+        />
+        <Button className='search-btn'
+        sx={{
+          bgcolor: '#FF2625',
+          color:'#fff',
+          textTransform:'none',
+          width: {lg:'20px',xs:'12px'}
+        }}>
+          
+        </Button>
+       </Box>
+    </Stack>
   )
 }
 
