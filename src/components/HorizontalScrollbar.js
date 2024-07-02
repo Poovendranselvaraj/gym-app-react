07ec977bfe163eee9967033a48128b94c27e1f22
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-02 17:31:46 +0530

    17.31

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Box } from '@mui/material';
+import {ScrollMenu, VisibilityContext} from 
+'react-horizontal-scrolling-menu';
 
 import BodyPart from './BodyPart';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
