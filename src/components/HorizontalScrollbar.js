f09a8b3034da9a7384275e14a2a4604884008754
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-03 14:03:31 +0530

    added

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -18,9 +18,9 @@ const LeftArrow = ()=> {
 };
 
 const RightArrow = ()=> {
-  const { scrollPrev } = useContext(VisibilityContext);
+  const { scrollNext } = useContext(VisibilityContext);
   return(
-    <Typography onClick={()=> scrollPrev()} className="left-arrow">
+    <Typography onClick={()=> scrollNext()} className="left-arrow">
       <img src={RightArrowIcon} alt="right-arrow"/>
     </Typography>
   );
@@ -28,13 +28,13 @@ const RightArrow = ()=> {
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <ScrollMenu LeftArrow={LeftArrow}RightArrow={RightArrow}>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item)=>(
             <Box
               key={item.id || item}
               itemId={item.id || item}  
               title={item.id || item}
-              m="0 40px"
+              m="0px 40px"
              >
                <BodyPart item={item} bodyPart=
                {BodyPart} setBodyPart=
