612a691dfdcd99f3068741cb30ccf5e1baa09b81
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-05 17:40:32 +0530

    2.15.14

--- src/Pages/ExerciseDetail.js
+++ src/Pages/ExerciseDetail.js
@@ -10,7 +10,10 @@ import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] =useState({});
-  const [exerciseVideos, setExerciseVideos] =useState([])
+  const [exerciseVideos, setExerciseVideos] =useState([]);
+  const [targetMuscleExercises, setTargetMuscleExercises]=useState([]);
+  const [equipmentExercises, setEquipmentExercises]=useState([]);
+
   const {id } = useParams();
 
   useEffect(() => { 
@@ -22,18 +25,23 @@ const ExerciseDetail = () => {
       setExerciseDetail(exerciseDetailData);
 
       const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,youtubeOptions);
-      setExerciseVideos(exerciseVideosData)
+      setExerciseVideos(exerciseVideosData.contents)
 
+      const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,exerciseOptions);
+      setTargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,exerciseOptions);
+      setEquipmentExercises(equipmentExercisesData);
     }
     fetchExercisesData();
   },[id]);
-
 
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail}/>
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
-      <SimilarExercises/>
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises}
+      equipmentExercises={equipmentExercises}/>
     </Box>  
   )
 }
