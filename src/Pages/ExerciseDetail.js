fdcc27a0e6ad5c3fb6cf29209d5c55bb4690bd50
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-05 13:47:38 +0530

    query

--- src/Pages/ExerciseDetail.js
+++ src/Pages/ExerciseDetail.js
@@ -18,12 +18,10 @@ const ExerciseDetail = () => {
       const exerciseDbUrl='https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
-      const exerciseDetailData=await fetchData(`${exerciseDbUrl}/exercises/exercise/$
-      {id}`,exerciseOptions);
+      const exerciseDetailData=await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
       setExerciseDetail(exerciseDetailData);
 
-      const exerciseVideosData = await fetchData(`{youtubeSearchUrl}/search?q=$
-      {exerciseDetailData.name}`,youtubeOptions);
+      const exerciseVideosData = await fetchData(`{youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,youtubeOptions);
       setExerciseVideos(exerciseVideosData)
 
     }
