26219d10f8131dbb4550294b73d7e93400eec8b1
poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-07-05 12:09:24 +0530

    error

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,12 +2,19 @@ export const exerciseOptions=  {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
-        'x-rapidapi-key': process.env.
-        REACT_APP_RAPID_API_KEY
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
           }
 };
     
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'youtube-search-and-download.p.rapidapi.com',
+      'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY
+    }
+  };
+ 
 
 export const fetchData=async (url, options) => {
     const response = await fetch(url,options);
